b19d8fd7cb8547e691533c277c2403ced17a15a7
Frans van Berckel <fberckel@xs4all.nl>
2020-12-28 13:49:02 +0100

    runner.sh: add ansible & dependencies

--- playbooks/tinkerbell/runner.sh
+++ playbooks/tinkerbell/runner.sh
@@ -9,4 +9,20 @@ EXTRA_VARS=$HOME/.ansible/.secrets.yml
 ANSIBLE_STDOUT_CALLBACK=debug
 PYTHONUNBUFFERED=1
 
+if ! command -v ansible >/dev/null; then
+        echo "Installing Ansible & dependencies"
+        if command -v sudo >/dev/null; then
+                if command -v apt-get >/dev/null; then
+                sudo apt-get update -qq
+                sudo apt-get install -y -qq openssh-client sshpass ansible
+                else
+                        echo "apt-get command not found!"
+                        exit 1
+                fi
+        else
+                echo "sudo command not found!"
+                exit 1
+        fi
+else
 ansible-playbook --verbose --extra-vars=@$EXTRA_VARS $GROUP_PLAYBOOK
+fi
